3a8ed16edd47b71da97e8ff5ff5c0bae149d0f04
Achinit99 <achinit720@gmail.com>
2023-05-17 14:15:43 +0530

    commets
         // Singular comments
    
            /* Multi
            * line
            * comments*/

--- java_basic1/src/Main.java
+++ java_basic1/src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! I'm Achini Kawshalya");
-        System.out.printf("I love myself");
+        System.out.print("Hello world! I'm Achini Kawshalya");
+        // Singular comments
+        /* Multi
+        * line
+        * comments*/
     }
 }
