a5fb1d0513df321e00f3f467c6a8fd660b8a1000
Achinit99 <achinit720@gmail.com>
2023-05-17 13:30:27 +0530

    println
    We can use println code for end code line

--- java_basic1/src/Main.java
+++ java_basic1/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello world! I'm Achini Kawshalya");
+        System.out.println("Hello world! I'm Achini Kawshalya");
+        System.out.printf("I love myself");
     }
 }
