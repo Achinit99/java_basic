502137b7e25a359515800bab60569bb467d5aa02
Achinit99 <achinit720@gmail.com>
2023-05-18 22:52:42 +0530

    print and println

--- java_basic1/src/Main.java
+++ java_basic1/src/Main.java
@@ -1,9 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello world! I'm Achini Kawshalya");
-        // Singular comments
-        /* Multi
-        * line
-        * comments*/
+        System.out.println("Hello world! I'm Achini Kawshalya");
+
     }
 }
