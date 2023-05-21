063a6b3635bc2b8e7a472960a98d58db20fad80a
Achinit99 <achinit720@gmail.com>
2023-05-22 00:15:50 +0530

    Simple Calculator

--- java_basic1/src/Main.java
+++ java_basic1/src/Main.java
@@ -1,10 +1,49 @@
-public class Main {
+import java.util.Scanner;
+
+class Main {
     public static void main(String[] args) {
-        int age=20;
-        int price=200;
 
-        System.out.println("My age is "+age);
-        System.out.println("It's price is "+price);
+        char operator;
+        Double number1, number2, result;
 
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("What is your operator: +, -, *, or /");
+        operator = input.next().charAt(0);
+
+        System.out.println("Enter your first number");
+        number1 = input.nextDouble();
+
+        System.out.println("Enter your second number");
+        number2 = input.nextDouble();
+
+        switch (operator) {
+
+            case '+':
+                result = number1 + number2;
+                System.out.println(number1 + " + " + number2 + " = " + result);
+                break;
+
+            case '-':
+                result = number1 - number2;
+                System.out.println(number1 + " - " + number2 + " = " + result);
+                break;
+
+            case '*':
+                result = number1 * number2;
+                System.out.println(number1 + " * " + number2 + " = " + result);
+                break;
+
+            case '/':
+                result = number1 / number2;
+                System.out.println(number1 + " / " + number2 + " = " + result);
+                break;
+
+            default:
+                System.out.println("Invalid operator!");
+                break;
+        }
+
+        input.close();
     }
 }
