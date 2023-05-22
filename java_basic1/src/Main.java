a7b48404bad364f7eb2ee9500419b59fecc94186
Achinit99 <achinit720@gmail.com>
2023-05-22 15:15:45 +0530

    How to get message

--- java_basic1/src/Main.java
+++ java_basic1/src/Main.java
@@ -1,49 +1,59 @@
-import java.util.Scanner;
+//import java.util.Scanner;
+//
+//class Main {
+//    public static void main(String[] args) {
+//
+//        char operator;
+//        Double number1, number2, result;
+//
+//        Scanner input = new Scanner(System.in);
+//
+//        System.out.println("What is your operator: +, -, *, or /");
+//        operator = input.next().charAt(0);
+//
+//        System.out.println("Enter your first number");
+//        number1 = input.nextDouble();
+//
+//        System.out.println("Enter your second number");
+//        number2 = input.nextDouble();
+//
+//        switch (operator) {
+//
+//            case '+':
+//                result = number1 + number2;
+//                System.out.println(number1 + " + " + number2 + " = " + result);
+//                break;
+//
+//            case '-':
+//                result = number1 - number2;
+//                System.out.println(number1 + " - " + number2 + " = " + result);
+//                break;
+//
+//            case '*':
+//                result = number1 * number2;
+//                System.out.println(number1 + " * " + number2 + " = " + result);
+//                break;
+//
+//            case '/':
+//                result = number1 / number2;
+//                System.out.println(number1 + " / " + number2 + " = " + result);
+//                break;
+//
+//            default:
+//                System.out.println("Invalid operator!");
+//                break;
+//        }
+//
+//        input.close();
+//    }
+//}
 
-class Main {
-    public static void main(String[] args) {
+import javax.swing.JOptionPane;
+public class Main {
 
-        char operator;
-        Double number1, number2, result;
-
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("What is your operator: +, -, *, or /");
-        operator = input.next().charAt(0);
-
-        System.out.println("Enter your first number");
-        number1 = input.nextDouble();
-
-        System.out.println("Enter your second number");
-        number2 = input.nextDouble();
-
-        switch (operator) {
-
-            case '+':
-                result = number1 + number2;
-                System.out.println(number1 + " + " + number2 + " = " + result);
-                break;
-
-            case '-':
-                result = number1 - number2;
-                System.out.println(number1 + " - " + number2 + " = " + result);
-                break;
-
-            case '*':
-                result = number1 * number2;
-                System.out.println(number1 + " * " + number2 + " = " + result);
-                break;
-
-            case '/':
-                result = number1 / number2;
-                System.out.println(number1 + " / " + number2 + " = " + result);
-                break;
-
-            default:
-                System.out.println("Invalid operator!");
-                break;
-        }
-
-        input.close();
+    public static void main(String args[])
+    {
+        String name = JOptionPane.showInputDialog("Hi! What is your name ? ");
+        JOptionPane.showMessageDialog(null , "I love you "+name+" !");
     }
 }
