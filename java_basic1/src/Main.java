9446802f3defdce799eeb3cb9ad03c2603a2831c
Achinit99 <achinit720@gmail.com>
2023-05-25 15:40:49 +0530

    while loop 1

--- java_basic1/src/Main.java
+++ java_basic1/src/Main.java
@@ -1,117 +1,117 @@
-//import java.util.Scanner;
-//
-//class Main {
-//    public static void main(String[] args) {
-//
-//        char operator;
-//        Double number1, number2, result;
-//
-//        Scanner input = new Scanner(System.in);
-//
-//        System.out.println("What is your operator: +, -, *, or /");
-//        operator = input.next().charAt(0);
-//
-//        System.out.println("Enter your first number");
-//        number1 = input.nextDouble();
-//
-//        System.out.println("Enter your second number");
-//        number2 = input.nextDouble();
-//
-//        switch (operator) {
-//
-//            case '+':
-//                result = number1 + number2;
-//                System.out.println(number1 + " + " + number2 + " = " + result);
-//                break;
-//
-//            case '-':
-//                result = number1 - number2;
-//                System.out.println(number1 + " - " + number2 + " = " + result);
-//                break;
-//
-//            case '*':
-//                result = number1 * number2;
-//                System.out.println(number1 + " * " + number2 + " = " + result);
-//                break;
-//
-//            case '/':
-//                result = number1 / number2;
-//                System.out.println(number1 + " / " + number2 + " = " + result);
-//                break;
-//
-//            default:
-//                System.out.println("Invalid operator!");
-//                break;
-//        }
-//
-//        input.close();
-//    }
-//}
+/* import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+
+        char operator;
+        Double number1, number2, result;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("What is your operator: +, -, *, or /");
+        operator = input.next().charAt(0);
+
+        System.out.println("Enter your first number");
+        number1 = input.nextDouble();
+
+        System.out.println("Enter your second number");
+        number2 = input.nextDouble();
+
+        switch (operator) {
+
+            case '+':
+                result = number1 + number2;
+                System.out.println(number1 + " + " + number2 + " = " + result);
+                break;
+
+            case '-':
+                result = number1 - number2;
+                System.out.println(number1 + " - " + number2 + " = " + result);
+                break;
+
+            case '*':
+                result = number1 * number2;
+                System.out.println(number1 + " * " + number2 + " = " + result);
+                break;
+
+            case '/':
+                result = number1 / number2;
+                System.out.println(number1 + " / " + number2 + " = " + result);
+                break;
+
+            default:
+                System.out.println("Invalid operator!");
+                break;
+        }
+
+        input.close();
+    }
+} */
 
 // Message box -----------------------
-//import javax.swing.JOptionPane;
-//public class Main {
-//
-//    public static void main(String args[])
-//    {
-//        String name = JOptionPane.showInputDialog("Hi! What is your name ? ");
-//        JOptionPane.showMessageDialog(null , "I love you "+name+" !");
-//    }
-//}
+/* import javax.swing.JOptionPane;
+public class Main {
+
+    public static void main(String args[])
+    {
+        String name = JOptionPane.showInputDialog("Hi! What is your name ? ");
+        JOptionPane.showMessageDialog(null , "I love you "+name+" !");
+    }
+} */
 
 //------------------------------------------
-//import java.util.Scanner;
-//
-//public  class Main {
-//    public static void main(String args[])
-//    {
-//        Scanner sun = new Scanner(System.in);
-//
-//        System.out.println(sun.nextLine()); //(sun.next())
-//    }
-//}
+/*import java.util.Scanner;
+
+public  class Main {
+    public static void main(String args[])
+    {
+        Scanner sun = new Scanner(System.in);
+
+        System.out.println(sun.nextLine()); //(sun.next())
+    }
+} */
 
 // ---------------------------------------
-//public class Main {
-//    public static void main(String args[])
-//    {
-//        int num1, num2, answer;
-//
-//        num1 = 11;
-//        num2 = 10;
-//
-//        answer = num1 + num2;
-//
-//        System.out.println(answer);
-//    }
-//}
+/*public class Main {
+    public static void main(String args[])
+    {
+        int num1, num2, answer;
+
+        num1 = 11;
+        num2 = 10;
+
+        answer = num1 + num2;
+
+        System.out.println(answer);
+    }
+} */
 
 // simple calculator --------------------------
-//import java.util.Scanner;
-//public class Main {
-//
-//    public static void main(String args[])
-//    {
-//        // ** Create scanner variable **
-//        Scanner sun = new Scanner(System.in);
-//
-//        // ** Create variable **
-//        double no1, no2, ans;
-//
-//        // ** Message user -> 01 **
-//        System.out.println("Enter your first number :");
-//        no1 = sun.nextDouble();
-//
-//        // ** Message user -> 02 **
-//        System.out.println("Enter your second number :");
-//        no2 = sun.nextDouble();
-//
-//        ans = no1 + no2;
-//
-//        System.out.println("Your answer is :"+ ans);
-//
-//    }
-//}
+/*import java.util.Scanner;
+public class Main {
+
+    public static void main(String args[])
+    {
+        // ** Create scanner variable **
+        Scanner sun = new Scanner(System.in);
+
+        // ** Create variable **
+        double no1, no2, ans;
+
+        // ** Message user -> 01 **
+        System.out.println("Enter your first number :");
+        no1 = sun.nextDouble();
+
+        // ** Message user -> 02 **
+        System.out.println("Enter your second number :");
+        no2 = sun.nextDouble();
+
+        ans = no1 + no2;
+
+        System.out.println("Your answer is :"+ ans);
+
+    }
+} */
 
 import java.sql.SQLOutput;
 
@@ -216,8 +216,17 @@ public class Main {
                 System.out.println("Your age is 18");
                 break;
             default:
-                System.out.println("I don't Know"); */
+                System.out.println("I don't Know");
+        } */
+
+        int count = 0;
+
+        while (){
+            
         }
+
+
+
 
     }
 }
